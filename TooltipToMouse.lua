d1e2596bb05c597677a514c690abb5a54c596f8b
Tarazhin <57341945+Tarazhin@users.noreply.github.com>
2021-03-31 23:51:55 +0200

    Update TooltipToMouse.lua

--- TooltipToMouse.lua
+++ TooltipToMouse.lua
@@ -1,6 +1,5 @@
 local frame = CreateFrame("FRAME"); -- Need a frame to respond to events
 frame:RegisterEvent("ADDON_LOADED"); -- Fired when saved variables are loaded
-frame:RegisterEvent("PLAYER_LOGOUT"); -- Fired when about to log out
 frame:SetScript("OnEvent", function(self, event, arg1)
     if event == "ADDON_LOADED" and arg1 == "TooltipToMouse" then
         print("AddOn geladen...")
@@ -13,13 +12,7 @@ frame:SetScript("OnEvent", function(self, event, arg1)
 end)
 SLASH_TTM1 = "/TTM"
 SlashCmdList['TTM'] = function(msg)
-    print(msg)
-    if string.find(msg, "off") then
-        TtM_IsActive = false
-    end
-    if string.find(msg, "on") then
-        TtM_IsActive = true
-    end
+    TtM_IsActive = not string.find(msg, "off")
 end
 
 function tooltipPos(s, p)
